b6fa86c0814c85341baca05f96e3aabb3fd78c68
kevindoni <kevindoni17@gmail.com>
2020-12-06 10:46:40 +0700

    Update embex.sh

--- embex.sh
+++ embex.sh
@@ -20,16 +20,16 @@ wget "https://github.com/kevindoni/DEBIAN9/blob/main/plugin.tgz"
 echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
 
 
-# set time GMT +8
+# set time GMT +7
 ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
 
 # install webmin
 cd
-wget "https://raw.githubusercontent.com/radzvpn/TNTNOLOADDNS/master/webmin_1.962_all.deb"
-dpkg --install webmin_1.962_all.deb;
+wget "https://github.com/radzvpn/VPSauto-1/blob/master/webmin_1.801_all.deb"
+dpkg --install webmin_1.801_all.deb;
 apt-get -y -f install;
 sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
-rm /root/webmin_1.962_all.deb
+rm /root/webmin_1.801_all.deb
 service webmin restart
 
 # install screenfetch
